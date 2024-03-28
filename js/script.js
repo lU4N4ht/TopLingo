5680b3ab54ea60c780cad6e6bebabdd55a1f3e61
Luana Magalhães <123824836+lU4N4ht@users.noreply.github.com>
2024-03-28 17:25:13 -0300

    tá indo

--- js/script.js
+++ js/script.js
@@ -1,40 +1,45 @@
 const fromText = document.querySelector(".from-text"),
-toText = document.querySelector(".to-text"),
-selectTag = document.querySelectorAll("select"),
-translateBtn = document.querySelector("button")
+    toText = document.querySelector(".to-text"),
+    selectTag = document.querySelectorAll("select"),
+    translateBtn = document.querySelector("button")
 
 selectTag.forEach((tag, id) => {
     for (let country_code in countries) {
-        let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "hi-IN" ? "selected" : "";
+        let selected = id == 0 ? country_code == "pt-BR" ? "selected" : "" : country_code == "en-GB" ? "selected" : "";
         let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
         tag.insertAdjacentHTML("beforeend", option);
     }
 });
 
 fromText.addEventListener("keyup", () => {
-    if(!fromText.value) {
+    if (!fromText.value) {
         toText.value = "";
     }
 });
 
+let toVoice = document.querySelector('.to')
+
+toVoice.addEventListener('click', function() {
+    let fromTalk;
+    fromTalk = new SpeechSynthesisUtterance(toText.value)
+    fromTalk.lang = selectTag[1].value;
+    speechSynthesis.speak(fromTalk)
+})
+
 translateBtn.addEventListener("click", () => {
     let text = fromText.value.trim(),
-    translateFrom = selectTag[0].value,
-    translateTo = selectTag[1].value;
-    if(!text) return;
+        translateFrom = selectTag[0].value,
+        translateTo = selectTag[1].value;
+    if (!text) return;
     toText.setAttribute("placeholder", "Translating...");
     let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
     fetch(apiUrl).then(res => res.json()).then(data => {
         toText.value = data.responseData.translatedText;
         data.matches.forEach(data => {
-            if(data.id === 0) {
+            if (data.id === 0) {
                 toText.value = data.translation;
             }
         });
         toText.setAttribute("placeholder", "Translation");
     });
 });
-
-
-
-
